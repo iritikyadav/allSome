d967d50f021c2aff0420b9d561f99ccf392c8551
iritikyadav123 <iritikyadav123@gmail.com>
2024-05-13 02:55:34 +0530

    add docker and docker composse file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-console.log("hello world");import express from 'express';
+import express from 'express';
+import mongoo from 'mongoose';
 const app = express();
 
-app.use(express.json());
-app.get('/', function(req,res) {
-    res.json("hello world");
+const url = process.env.MONGO_URL || "mongo://localhost:27017"
+
+mongoo.connect(url);
+
+const userSchema = new mongoo.Schema({
+    email : String,
+    password: String
 })
+const user = mongoo.model('User', userSchema);
+
+app.use(express.json());
+app.get('/', async function(req,res) {
+    const users = await user.find({});
+    res.json({
+        user
+    })
+})
+
+app.post('/', async function(req,res) {
+    const {email, password} = req.body
+    const users = await user.create({
+        email,
+        password
+    })
+})
+
+app.listen(3000);
