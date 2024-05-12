00365d37b5a836516ff7fa23804751be6ecacca0
iritikyadav123 <iritikyadav123@gmail.com>
2024-05-13 01:36:50 +0530

    ass sic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 console.log("hello world");import express from 'express';
 const app = express();
 
-app.use(express.json());
+//app.use(express.json());
 app.get('/', function(req,res) {
     res.json("hello world");
 })
